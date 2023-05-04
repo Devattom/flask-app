89651ccbfc4d1fecb2a9f5fbc1958be3815b52dc
Rousselin <devattom@gmail.com>
2023-05-04 21:27:28 +0200

    ajout des contrôle de champs sur le form

--- app.py
+++ app.py
@@ -37,25 +37,30 @@ def signupRoute():
         user_email = request.form['mail']
         psw1 = request.form['psw1']
         psw2 = request.form['psw2']
-        message = ''
+        message = {}
 
-        regex = '[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[a-z]{2,4}'
+        regex_mail = '[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[a-z]{2,4}'
+        regex_names = '[A-za-z._%-]'
+    
+        if not re.fullmatch(regex_mail, user_email):
+            message['email'] = "Veuillez rentrer un email au format valide"
+        if not re.fullmatch(regex_names, user_name):
+            message['name'] = "Veuillez rentrer un nom valide"
+        if not re.fullmatch(regex_names, user_firstname):
+            message['firstname'] = "Veuillez rentrer un prénom valide"
 
-        if re.fullmatch(regex, user_email):
-            if psw1 == psw2:
-                db = Bdd.Db()
-                if db.getUserByEmail(user_email):
-                    message = "Un compte existe déjà avec cet Email"
-                else:
-                    salt = bcrypt.gensalt(rounds = 15)
-                    crypt_psw = bcrypt.hashpw(psw1.encode('utf-8'), salt)
-                    db.insertUser(user_name, user_firstname, user_email, crypt_psw)
-                    retour = db.getData()
-                    message = 'psw ok'
+        if psw1 and psw1 == psw2:
+            db = Bdd.Db()
+            if db.getUserByEmail(user_email):
+                message['user'] = "Un compte existe déjà avec cet Email"
             else:
-                message = 'Veuillez Saisir deux mots de passe identique'
+                salt = bcrypt.gensalt(rounds = 15)
+                crypt_psw = bcrypt.hashpw(psw1.encode('utf-8'), salt)
+                db.insertUser(user_name, user_firstname, user_email, crypt_psw)
         else:
-            message = "Veuillez rentrer un email au format valide"
+            message['password'] = 'Veuillez Saisir deux mots de passe identiques'
+
+            
         return render_template('main.html',
                            title = "SignUp",
                            path = path,
